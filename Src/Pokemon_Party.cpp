6c947a174d23b25ae39ef0f9bfdca93ea690fa61
mezzi <mezzieya665@gmail.com>
2026-09-14 14:49:52 +0200

    Ajout de Pokemon Party

--- Src/Pokemon_Party.cpp
+++ Src/Pokemon_Party.cpp
@@ -3,3 +3,37 @@
 //
 
 #include "../Inc/Pokemon_Party.h"
+
+#include <iostream>
+
+void PokemonParty::ajouterPokemon(const Pokemon& p) {
+    liste.push_back(p);
+}
+
+Pokemon* PokemonParty::getPokemonByNumero(int numero) {
+    for (auto& p : liste) {
+        if (p.getNumero() == numero) {
+            return new Pokemon(p);
+        }
+    }
+    return nullptr;
+}
+
+Pokemon* PokemonParty::getPokemonByNom(const std::string& nom) {
+    for (auto& p : liste) {
+        if (p.getNom() == nom) {
+            return new Pokemon(p);
+        }
+    }
+    return nullptr;
+}
+
+bool PokemonParty::retirerPokemon(int numero) {
+    for (auto it = listePockemons.begin(); it != listePockemons.end(); ++it) {
+        if (it->getNumero() == numero) {
+            liste.erase(it);
+            return true;
+        }
+    }
+    return false;
+}
